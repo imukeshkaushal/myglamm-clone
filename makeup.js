0b9926191e294b6e9d62d8887763e57da9974806
Mukesh Kaushal <imukeshkaushal@gmail.com>
2022-07-21 21:11:04 +0530

    makeup page added

---
+++ makeup.js
@@ -0,0 +1,191 @@
+let makeupData = [
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Twstr-(1).jpg",
+        heading : "TWIST IT MASCARA",
+        desc : "Volumising and Curling Mascara",
+        price :645,
+        shortPrice :"",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Instabdd-(1).jpg",
+        heading : "MYGLAMM LIT LIQUID MATTE LIPSTICK - INSTA BADDIE",
+        desc : "Moringa Oil Enriched Matte Lipstick",
+        price :345,
+        shortPrice :"",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Artboard-1-(1)-(2).jpg",
+        heading : "MYGLAMM SUPERFOODS KAJAL",
+        desc : "Long-Wear Smudge-Proof Kajal Infused With Natural Superfood Extracts",
+        price :296,
+        shortPrice :"₹ 395",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Beat-(1)_1.jpg",
+        heading : "MYGLAMM LIT RADIANT MATTE COMPACT POWDER - BEAT",
+        desc : "Illuminating Matte Pressed Powder",
+        price :371,
+        shortPrice :"₹ 495",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Open-Product-with-side-bar_1.jpg",
+        heading : "POPXO MAKEUP - POWER TRIP - MINI LIP KIT",
+        desc : "Mini Lipstick kit with 3 creamy-matte bold lipsticks",
+        price :244,
+        shortPrice :"₹ 349",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/4-(1)jpg.jpg",
+        heading : "POPXO MAKEUP - DREAMIN’ MINI NAIL KIT",
+        desc : "Mini Nail Polish set with 5 chip-resistant glitter nail polishes",
+        price :186,
+        shortPrice :"₹ 249",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/RSEAD-(1).jpg",
+        heading : "MYGLAMM LIT - PH LIP BALM - ROSE ALL DAY",
+        desc : "Creamy, Hydrating Formula with a Luminous Effect",
+        price :345,
+        shortPrice :"",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Fakeid-(1).jpg",
+        heading : "MYGLAMM LIT VELVET MATTE LIQUID LIPSTICK - FAKE ID",
+        desc : "Hydrating, Velvet Matte Liquid Lipcolour",
+        price :395,
+        shortPrice :"",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/open-product-with-side-bar_5.jpg",
+        heading : "POPXO 12 EYESHADOW KIT - SQUAD GOALS",
+        desc : "Eyeshadow palette with 6 matte and 6 shimmer shades",
+        price :314,
+        shortPrice :"₹ 449",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Party-parade-(1).jpg",
+        heading : "MYGLAMM TWO OF YOUR KIND NAIL ENAMEL- PARTY PARADE",
+        desc : "Long-Lasting Gel Finish Nail Enamel Set",
+        price :221,
+        shortPrice :"₹ 295",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Cosmo-(1).jpg",
+        heading : "PERFECT CURVES MATTE LIP CRAYON - COSMOPOLITAN",
+        desc : "Moisturising Matte Lipstick",
+        price :486,
+        shortPrice :"₹ 695",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/6-Precision-Liquid-Felt-Tip-Eyeliner-post-1-Black-Diamond.jpg",
+        heading : "MANISH MALHOTRA PRECISION LIQUID FELT TIP EYELINER - BLACK DIAMOND",
+        desc : "Intense Longwear Liquid Eyeliner Pen | Made in Germany",
+        price :671,
+        shortPrice :"₹ 895",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/CHRls-(1).jpg",
+        heading : "MYGLAMM K.PLAY FLAVOURED LIPSTICK - CHERRY BURST",
+        desc : "Cherry Flavoured Vitamin E-enriched Creamy Lipstick",
+        price :545,
+        shortPrice :"",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/8-Mascara-post-1-Black_1.jpg",
+        heading : "MANISH MALHOTRA MASCARA",
+        desc : "Classic Volumizing & Defining Mascara",
+        price :637,
+        shortPrice :"₹ 850",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/GRENSR-(1)_1.jpg",
+        heading : "MYGLAMM LIT GLOSSY LIQUID EYELINER - GREEN SOIRÉE",
+        desc : "Up To 24HR Smudge-Proof Coloured Eyeliner",
+        price :325,
+        shortPrice :"",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/open-product-with-side-bar_4.jpg",
+        heading : "POPXO MAKEUP - SEND NOODS - 4 EYESHADOW KIT",
+        desc : "Eyeshadow palette with 2 shimmer and 2 matte shades",
+        price :224,
+        shortPrice :"₹ 299",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Fvr-(1).jpg",
+        heading : "TINT IT UP - FEVER",
+        desc : "Lip & Cheek Stain",
+        price :695,
+        shortPrice :"",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/BRDF-(1)_1.jpg",
+        heading : "MYGLAMM LIT BROW DEFINER PENCIL",
+        desc : "Define & Sculpt Browliner",
+        price :495,
+        shortPrice :"",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/open-product-side-bar.jpg",
+        heading : "POPXO MAKEUP - INSTAGLAMM FACE KIT",
+        desc : "3-in-1 blush, setting powder, and highlighter palette",
+        price :279,
+        shortPrice :"₹ 399",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Banan-(1).jpg",
+        heading : "MYGLAMM POSE HD BANANA POWDER - YELLOW",
+        desc : "HD Yellow Setting Powder",
+        price :524,
+        shortPrice :"₹ 499",
+    },
+    {
+        img : "https://files.myglamm.com/site-images/800x800/Banan-(1).jpg",
+        heading : "MYGLAMM POSE HD BANANA POWDER - YELLOW",
+        desc : "HD Yellow Setting Powder",
+        price :524,
+        shortPrice :"₹ 499",
+    }
+    
+]
+
+localStorage.setItem("makeupArr",JSON.stringify(makeupData));
+console.log(makeupData)
+
+let makeupList = JSON.parse(localStorage.getItem("makeupArr")) || [];
+let cartData = JSON.parse(localStorage.getItem("cartArr"))||[];
+
+makeupList.map(function(el,index){
+    let div = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = el.img;
+
+    let heading = document.createElement("h3");
+    heading.innerText = el.heading;
+    let desc = document.createElement("p");
+    desc.innerText = el.desc;
+    
+    let div1 = document.createElement("div");
+    div1.setAttribute("class","price-flex")
+    let price = document.createElement("p");
+    price.innerText = "₹ " +el.price;
+    let sortPrice = document.createElement("p");
+    sortPrice.setAttribute("class","cutPrice")
+    sortPrice.innerText = el.shortPrice;
+    let btn = document.createElement("p");
+    btn.setAttribute("class","cartBtn");
+    btn.innerText = "+"
+    btn.addEventListener("click",function(){
+        cartFunction(el,index);
+    })
+
+    div1.append(price,sortPrice,btn);
+    
+    div.append(image,heading,desc,div1);
+    document.querySelector("#makeup-div").append(div);
+})
+
+function cartFunction(el){
+    cartData.push(el);
+    localStorage.setItem("cartArr",JSON.stringify(cartData));
+}
